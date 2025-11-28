13a914d9bb7a909d896554f0131b09f9631bbdd5
Jose Rohit M <116860412+joserohit264@users.noreply.github.com>
2025-11-29 01:48:25 +0530

    Delete Secure_Password_Manager directory

--- Secure_Password_Manager/backend/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-from .user import User
-from .password import Password
--- Secure_Password_Manager/start_app.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Starting Secure Password Manager...
-cd backend
-pip install -r requirements.txt
-python app.py
-pause
